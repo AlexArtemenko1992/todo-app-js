c30135c497997a9648e9ac65e9ac9465524310a3
Александр Артеменко <aleksandrartemenko@MacBook-Air-Aleksandr.local>
2020-11-01 12:53:10 +0200

    fix styles

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
+const container = document.querySelector(".container");
 const form = document.querySelector(".form");
 const inputText = document.querySelector(".input-text");
 const activeList = document.querySelector(".active-list");
 const doneList = document.querySelector(".done-list");
 
- // Array of tasks
+// Array of tasks
 const taskList = [
   { id: 1, text: "Купить яблоки" },
   { id: 2, text: "Забрать деньги" },
 ];
 
- // Serialize array to obj
+// Serialize array to obj
 const objOfTasks = taskList.reduce((acc, el) => {
   acc[el.id] = el;
   return acc;
 }, {});
 
-  // Obj of done tasks
+// Obj of done tasks
 const doneTasksList = {};
 
-  // Events
+// Events
 form.addEventListener("submit", onSubmitHandler);
 document.addEventListener("DOMContentLoaded", generateStaticTask(objOfTasks));
 
-  // Generate static tasks
+// Generate static tasks
 function generateStaticTask(objOfTasks) {
   Object.values(objOfTasks).forEach((task) => {
     const template = `
@@ -41,7 +42,7 @@ function generateStaticTask(objOfTasks) {
   });
 }
 
-  // Submit Handler
+// Submit Handler
 function onSubmitHandler(e) {
   e.preventDefault();
   const newTask = inputText.value;
@@ -49,7 +50,7 @@ function onSubmitHandler(e) {
   form.reset();
 }
 
-  // Add new task to obj and HTML.
+// Add new task to obj and HTML.
 function addNewTaskToObj(task) {
   if (!task) {
     return;
@@ -64,7 +65,7 @@ function addNewTaskToObj(task) {
   addToDoneList(objOfTasks);
 }
 
-  // Generate new task
+// Generate new task
 function generateTask(task) {
   const newTaskTemplate = `
     <li class="active-list_item col-12 m-auto" id='${task.id}'>
@@ -79,7 +80,7 @@ function generateTask(task) {
   activeList.insertAdjacentHTML("afterbegin", newTaskTemplate);
 }
 
-  // Delete handler
+// Delete handler
 function deleteTask(objOfTasks) {
   const delBtn = document.querySelector(".btn-delete");
   delBtn.addEventListener("click", (e) => {
@@ -94,7 +95,7 @@ function deleteTask(objOfTasks) {
   });
 }
 
-  // Add task to done-list
+// Add task to done-list
 function addToDoneList(objOfTasks) {
   const allLi = document.querySelectorAll(".active-list_item");
   allLi.forEach((el) => {
